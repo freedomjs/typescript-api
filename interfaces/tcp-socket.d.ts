db27fdecba8bc6b530bd54e4b230b89331394adb
Lucas Dixon <ldixon@google.com>
2014-06-05 22:53:14 -0700

    gave correct typing to -event handling functions

--- interfaces/tcp-socket.d.ts
+++ interfaces/tcp-socket.d.ts
@@ -24,16 +24,31 @@ declare module freedom.TcpSocket {
     peerPort :number;
     localAddress :string;
   }
+
+  export interface OnConnectInfo {
+    socket :number;
+    host :string;
+    port :number;
+  }
+
+  export interface OnDisconnectInfo {
+    errcode :string;
+    message :string;
+  }
 }  // module Sockets
 
 // The TcpSocket class (freedom['core.TcpSocket'])
 declare module freedom {
   export interface TcpSocket {
     listen(address:string, port:number) : Promise<void>;
-    connect(hostname :string, port :number) : Promise<void>;
-    write(data :ArrayBuffer) : Promise<freedom.TcpSocket.WriteInfo>;
-    getInfo() : Promise<freedom.TcpSocket.SocketInfo>;
+    connect(hostname:string, port:number) : Promise<void>;
+    write(data:ArrayBuffer) : Promise<TcpSocket.WriteInfo>;
+    getInfo() : Promise<TcpSocket.SocketInfo>;
     close() : Promise<void>;
-    on(type: string, callback: Function) : void;
+    // TcpSockets have 3 types of events:
+    on(eventType:string, f:Function) : void;
+    on(type:'onConnection', f:(i:TcpSocket.OnConnectInfo) => void) : void;
+    on(type:'onData', f:(i:TcpSocket.ReadInfo) => void) : void;
+    on(type:'onDisconnect', f:(i:TcpSocket.OnDisconnectInfo) => void) : void;
   }
 }
