ddd8f2d8014020cdcbb1f19e66e66f843424d6e3
Lucas Dixon <ldixon@google.com>
2014-06-19 00:30:42 -0400

    significant cleanup

--- interfaces/peer-connection.d.ts
+++ interfaces/peer-connection.d.ts
@@ -1,8 +1,8 @@
-// TODO: Make these actually typed, and probably shove into freedom.
+// This documents the peerconnection core provider who's freedom-interface is
+// here: https://github.com/freedomjs/freedom/blob/master/interface/core.js
 
-// Reference:
-// https://github.com/UWNetworksLab/freedom/blob/master/interface/core.js
 /// <reference path='../../third_party/promise/promise.d.ts' />
+/// <reference path='freedom.d.ts' />
 
 declare module freedom.PeerConnection {
   interface ChannelInfo {
@@ -24,29 +24,32 @@ declare module freedom {
 
   // TODO: add issue for `onOpenDataChannel` to have return value field named
   // `channelLabel`, not `channelId`, to be consistent with `onReceived`.
-  class PeerConnection {
+  interface PeerConnection {
     // Setup a new peer connection.
-    setup(freedomChannelId:string, // Freedom signalling channel id.
-            debugName:string,  // used for debugging messages.
-            stunServers:string[])
-          : Promise<void>;
+    setup(freedomChannelId  :freedom.ChannelEndpointIdentifier,
+          debugName:string,  // used for debugging messages.
+          stunServers:string[])
+        : Promise<void>;
 
     // Send a message, if the channelLabel does not exist, it is created. TODO:
     // clarify semantics: does a channel created by this class raise an
     // `onOpenDataChannel` event?
     send(d:PeerConnection.ChannelMessage) : void;
-    openDataChannel(channelLabel:string) : void;
-    closeDataChannel(channelLabel:string) : void;
-    close() : void;
+    openDataChannel(channelLabel:string) : Promise<void>;
+    closeDataChannel(channelLabel:string) : Promise<void>;
+    close() : Promise<void>;
 
     // Generic freedom `on` handler.
     on(t:string, f:Function) : void;
-    on(t:'onOpenDataChannel', f:(d:PeerConnection.ChannelInfo) => void) : void;
     on(t:'onReceived', f:(d:PeerConnection.ChannelMessage) => void) : void;
     on(t:'onClose', f:() => void) : void;
+    // The |onOpenDataChannel| and |onCloseDataChannel| events happen when the
+    // remote peer opens or closes a connection to this peer-connection.
+    on(t:'onOpenDataChannel', f:(d:PeerConnection.ChannelInfo) => void) : void;
     on(t:'onCloseDataChannel', f:(d:PeerConnection.ChannelInfo) => void) : void;
 
-    // Given a channel Label, returns the buffered amount on that channel.
+    // Given a channel Label, returns the buffered amount on that channel. TODO:
+    // this should probably not be exposed. buffering should be handled inside.
     getBufferedAmount(string) : Promise<number>;
   }
 }
